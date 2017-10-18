3f02cbd6eb4b2e075a27bfa9afdba73fd2ce1931
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-18 13:47:08 -0500

    adding copyright

--- footer.php
+++ footer.php
@@ -7,8 +7,14 @@
 
 <?php get_sidebar(); ?>
 
-<footer class="copyright">Newt theme, ©2017 | Developed by:  Rudolph Schmitz</footer>
-<?php wp_footer(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 d-flex justify-content-center">
+            <footer class="copyright">Newt theme, ©2017 | Developed by:  Rudolph Schmitz</footer>
+            <?php wp_footer(); ?>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
