b62ce14f930844e5c583ea0ec9ef8c7801c96a20
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-18 13:18:12 -0500

    updating nav

--- header.php
+++ header.php
@@ -14,6 +14,29 @@
 <?php wp_head(); ?>
 </head>
 
-<nav class="newt-nav-main">
+    <!-- Navigation start -->
+    <div class="no-gutters pb-5">
+        <nav class="nav fixed-top navbar-expand-lg nav-fill nav-style">
+
+            <!-- Navbar brand -->
+            <a class="navbar-brand nav-btn" id="goHomeBrand">
+                RS
+            </a>
+
+            <!-- Button responsible for opening/closing navbar on mobile -->
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarInfo" aria-controls="navbarInfo" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">MENU</span>
+            </button>
+
+            <!-- Actual navbar links -->
+            <div class="collapse navbar-collapse" id="navbarInfo">
+                <a class="nav-link nav-item" data-toggle="collapse" data-target=".navbar-collapse.show"><button id="goHome" class="nav-btn underline">Home <span class="sr-only">(current)</span></button></a>
+                <a class="nav-link nav-item" data-toggle="collapse" data-target=".navbar-collapse.show"><button id="goAbout" class="nav-btn underline">About</button></a>
+                <a class="nav-link nav-item" data-toggle="collapse" data-target=".navbar-collapse.show"><button id="goProjects" class="nav-btn underline">Projects</button></a>
+                <a class="nav-link nav-item" data-toggle="collapse" data-target=".navbar-collapse.show"><button id="goContact" class="nav-btn underline">Contact</button></a>
+            </div>
+        </nav>
+    </div>
+    <!-- Navigation end -->
 <?php wp_nav_menu(); ?>
 </nav>
