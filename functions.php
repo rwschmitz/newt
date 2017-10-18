d0f0ad6e22e56c61dc118fe9d1e446e10012b47d
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-18 13:05:00 -0500

    adding scroll.js

--- functions.php
+++ functions.php
@@ -11,12 +11,13 @@ function modify_wordpress_jquery(){
 add_action('init', 'modify_wordpress_jquery');
 
 function newt_scripts() {
-	wp_enqueue_style( 'custom-css', get_template_directory_uri() . '/css/style.css');
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
 	wp_enqueue_style('bootstrap-v4', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css');
+	wp_enqueue_style( 'custom-css', get_template_directory_uri() . '/css/style.css');
 	wp_enqueue_style('raleway-font', 'https://fonts.googleapis.com/css?family=Raleway');
 	wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js');
 	wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js');
+	wp_enqueue_script('scroll', get_template_directory_uri() . '/js/scroll.js');
 }
 
 add_action( 'wp_enqueue_scripts', 'newt_scripts');
