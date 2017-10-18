96f6380fb93f483ebb842f83514f0fd530c35acb
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-18 13:23:29 -0500

    adjusting font family

--- index.php
+++ index.php
@@ -1,6 +1,162 @@
 <?php get_header(); ?>
 <div class="banner"></div>
-<div class="main-content">
+
+
+  <!-- About start -->
+  <div id="about" class="container">
+        <div class="row">
+            <div class="d-flex justify-content-center col-xl-12 header py-5">
+                About
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-center col-xl-12 content text-justify">
+                Hey there! I'm sure you've gathered by now my name is Rudolph Schmitz. Or you haven't and now you have! Alternatively, you can call me Rudy. Whichever is easiest for you to remember. One is a famous reindeer's name, the other is a widely known movie about a football player. Both unique enough to keep track of. For those curious, no, I still haven't seen the movie Rudy. Besides having the ability to write short paragrahs about my name, I've also obtained the following credentials:
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-center col-xl-12 py-5 subheader">
+                Education/Experience
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-start col-xl-4 py-3 content">
+                <i class="fa fa-graduation-cap pr-2 icons" aria-hidden="true"></i>B.S. Electrical Engineering - Missouri University of Science &amp; Technology (formerly University of Missouri - Rolla)
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-start col-xl-4 py-3 content">
+                <i class="fa fa-graduation-cap pr-2 icons" aria-hidden="true"></i>M.S. Engineering Management - Missouri University of Science &amp; Technology (formerly University of Missouri - Rolla)
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-start col-xl-4 py-3 content">
+                <i class="fa fa-wrench pr-3 pr-xl-2 icons" aria-hidden="true"></i>~3.5 years as a Commissioning Engineer performing Controls Engineering for Intelligrated/Honeywell
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-center col-xl-12 py-5 subheader">
+                Skills
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-start icon-html5 icons px-2"></i>
+                    <div class="text-center py-2">
+                        HTML5
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-center icon-css3 icons px-2"></i>
+                    <div class="text-center py-2">
+                        CSS3
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-end icon-sass icons px-2"></i>
+                    <div class="text-center py-2">
+                        Sass
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-start icon-javascript icons px-2 ml-3 ml-xl-2"></i>
+                    <div class="text-center py-2">
+                        JavaScript
+                    </div>
+                </div>
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-center icon-jquery icons px-2"></i>
+                    <div class="text-center py-2">
+                        jQuery
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-end icon-nodejs icons px-2"></i>
+                    <div class="text-center py-2">
+                        node.js
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-start icon-grunt icons px-2 ml-xl-1"></i>
+                    <div class="text-center py-2">
+                        Grunt
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-center icon-php icons px-2"></i>
+                    <div class="text-center py-2">
+                        PHP
+                    </div>
+                </div>
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+
+        <div class="row">
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-end icon-bootstrap icons px-2 mr-2 mr-xl-2"></i>
+                    <div class="text-center py-2">
+                        Bootstrap
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-start icon-git icons px-2"></i>
+                    <div class="text-center py-2">
+                        Git
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-center icon-wordpress icons px-2"></i>
+                    <div class="text-center py-2">
+                        Wordpress
+                    </div>
+                </div>
+            </div> <!-- End col -->
+
+            <div class="d-flex justify-content-center col-3 col-xl-3 py-3">
+                <div class="content">
+                    <i class="d-flex justify-content-end icon-apache icons px-2"></i>
+                    <div class="text-center py-2">
+                        Apache
+                    </div>
+                </div>
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+    </div> <!-- End container -->
+    <!-- About end -->
 
 
     <h1>new test</h1>
